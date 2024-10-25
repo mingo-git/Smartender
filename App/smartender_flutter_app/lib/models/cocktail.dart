3d712d3403672230125d9fd26a84cb368ddb1d62
Jonas <jonas.eck55@gmail.com>
2024-10-25 20:00:36 +0200

    Registerpage implemented and Dev-Branch added

---
+++ App/smartender_flutter_app/lib/models/cocktail.dart
@@ -0,0 +1,7 @@
+class Cocktail{
+  final String name;
+  final String[] ingredients;
+  final String imagePath;
+
+  Cocktail({required this.name, required this.ingredients, required this.imagePath})
+}
