dc2bc934977af4e1e13dcdbe7be81a7ae696beb4
Constantin Thein <constantin.thein@gmail.com>
2025-01-10 14:58:33 +0100

    feat(hardware): :sparkles: Added Logger and logging

--- Hardware/Rasp/application/main.py
+++ Hardware/Rasp/application/main.py
@@ -5,8 +5,15 @@ from modules.position_handler import PositionHandler
 from modules.led_controller import LEDController
 from modules.error_handler import ErrorHandler
 from modules.command_mapper import CommandMapper
+from modules.utils.logger import Logger
 
 def main():
+  logger = Logger()
+  # Log-Einträge erzeugen
+  logger.log("INFO", "Die Anwendung wurde gestartet", "Main")
+  logger.log("ERROR", "Ein Fehler ist aufgetreten", "Database")
+  logger.log("DEBUG", "Debugging des Moduls", "ModuleA")
+  logger.log("ALERT", "Kritischer Zustand erkannt", "SystemMonitor")
   print(main)
 
 if __name__ == "__main__":
